909afe03f45a8cb41a8bc7c199e1aae49cde2564
Solis13 <156252616+Solis13@users.noreply.github.com>
2024-11-13 21:17:54 -0600

    lab 11

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
